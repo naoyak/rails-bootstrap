ea56cb3479b7c78ff5727b2cf0b4df1efdeb2774
Naoya Kanai <naopon@gmail.com>
2013-07-26 16:12:21 -0700

    add form to home page

---
+++ app/models/visitor.rb
@@ -0,0 +1,8 @@
+class Visitor < ActiveRecord::Base
+	has_no_table
+	column :favorite, :string
+	column :comment, :string
+	validates_presence_of :favorite
+
+	IMAGE_LABELS = ['San Francisco', 'Sydney', 'Paris', 'Salar de Uyuni']
+end
